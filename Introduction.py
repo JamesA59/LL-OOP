e6096c6e074ebcaeb2154a6304c37dd9d4a1aa66
James Allen <jamesba555@yahoo.com>
2024-06-26 14:52:02 -0500

    final save?

--- Introduction.py
+++ Introduction.py
@@ -1,2 +1,19 @@
 # Introduction
 
+# The strengths of PYthon are its power and simplicity
+# OOP helps keep programs organized, extensible, and easy to adapt and change 
+# as projects get longer and more complex and new team members are brought on
+
+# What you should already know:
+# Working with IDEs and text editors, development tools
+#       Ex. Visual Studio Code
+# Python programming fundamentals
+#       Ex. how to writing functions, using conditional statements
+# Basic object-oriented design principles
+#       Ex. classes and methods
+
+# Setting up the development environment:
+# Has a github repository of start and finish examples:
+# github.com/LinkedInLearning/python-object-oriented-programming-4413110/tree/
+# I am using Python 3.9.6, not sure if that is up to date enough
+#]
