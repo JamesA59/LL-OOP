e1db7a14cdc2b094c4bd58d30bbc86870b8362a7
James Allen <jamesba555@yahoo.com>
2024-06-27 14:42:57 -0500

    done

--- OOP-Quiz.py
+++ OOP-Quiz.py
@@ -1,1 +1,23 @@
 # OOP- Quiz
+
+# What is the purpose of the self parameter within instance methods of a class?
+# to refer to the instance of the class on which the method is called
+
+# What is the primary purpose of the init method?
+# to initialize the instance content such as properties and variables
+
+# What is the primary difference between the type() function and the isinstance() function when checking the type of an object?
+# type() returns the specific class or type of an object, 
+# while isinstance() checks if an object is an instance of a particular class or its subclasses
+
+# What is the correct code to determine if an instance of a class is either a MyClass object or a descendant of one?
+# result = isinstance(my_obj, MyClass)
+
+# What is an instance method?
+# a method that can be called on a specific instance of a class
+
+# What is an instance attribute in Python?
+# a variable that holds data specific to an instance of a class
+
+# What is the key difference between class variables and instance variables?
+# Class variables are shared among all instances of the class, while instance variables are unique to each instance.
