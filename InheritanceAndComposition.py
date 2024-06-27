3d2f38845dfba4c5f663bd1ec132b0af2db27869
James Allen <jamesba555@yahoo.com>
2024-06-27 15:20:52 -0500

    wip1

--- InheritanceAndComposition.py
+++ InheritanceAndComposition.py
@@ -1,1 +1,51 @@
 # Inheritance and Composition
+
+# Understanding inheritance:
+
+# The original code here is messy because of how needlessly long it is, inheritance can make the code more concise
+# Adding Publication and Periodical classes to become base classes to the other 3 classes and to comment out the unnecessary lines
+class Publication:
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
+class Periodical(Publication):
+    def __init__(self,title, price, period, publisher):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
+        #self.title = title
+        #self.price = price
+        self.author = author
+        self.pages = pages
+
+
+class Magazine(Periodical):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, price, period, publisher)
+        #self.title = title
+        #self.price = price
+        #self.period = period
+        #self.publisher = publisher
+
+
+class Newspaper(Periodical):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, price, period, publisher)
+        #self.title = title
+        #self.price = price
+        #self.period = period
+        #self.publisher = publisher
+
+
+b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
+n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
+m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
+
+print(b1.author)
+print(n1.publisher)
+print(b1.price, m1.price, n1.price)
