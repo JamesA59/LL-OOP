e55ab96e1b327f2d55b55774d2a5eae9eebfc304
James Allen <jamesba555@yahoo.com>
2024-07-01 10:06:15 -0500

    done

--- InheritanceAndCompositionQuiz.py
+++ InheritanceAndCompositionQuiz.py
@@ -1,1 +1,22 @@
 # Inheritance and Composition Quiz
+
+# In the case of multiple inheritance, if two base classes have methods with the same name,
+#  and a derived class calls this method, which method will be executed?
+
+# The method of the first base class listed in the inheritance chain will be executed.
+
+# What is multiple inheritance in Python?
+# It is the ability to inherit from multiple classes simultaneously.
+
+# In Python, how do you define an Abstract Base Class using the abc module?
+# by creating a class that inherits from the abc.ABC base class
+
+# What is object composition in Python, and how does it differ from inheritance?
+# Object composition is a way to create new objects by combining them using references to other objects, 
+# while inheritance is a way to create new classes by inheriting attributes and methods from other classes.
+
+# What is the primary purpose of creating and using an interface?
+# to enforce that classes inheriting from the interface must implement certain methods
+
+# What is the correct syntax for calling a base class method from another Python method in a subclass?
+# super().method_name()
