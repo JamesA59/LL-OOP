f2996548852a0c5a69a37277f81da167016614dc
James Allen <jamesba555@yahoo.com>
2024-07-01 14:15:44 -0500

    wip1

--- MagicObjectMethods.py
+++ MagicObjectMethods.py
@@ -1,1 +1,12 @@
 # Magic Object Methods
+
+# What are magic methods?:
+
+# A set of methods that Python automatically associates with every class definition
+# Customize object behavior and integrate with the language
+# Degine how objects are represented as strings
+# Control access to attirbute values, both for get and set
+# Build in comparison and equality testing capabilities
+# Allow objects to be called like functions
+# Can make code more concise and readable
+# Features like these are what Python its flexibility and power
