8e5725fcb73644c0e8cd15c0b21ae00e3874d4a7
James Allen <jamesba555@yahoo.com>
2024-07-11 15:18:31 -0500

    done

--- MagicMethodsQuiz.py
+++ MagicMethodsQuiz.py
@@ -1,1 +1,14 @@
 # Magic Methods Quiz
+
+# 1. What is the main purpose of the eq method when defining it in a class?
+# to compare two objects for structural equality (whether their contents are the same)
+
+# 2. What is the primary purpose of the call method in a Python class?
+# to allow instances of the class to be called like functions
+
+# 3. What is the primary purpose of the getattr method in Python classes?
+# to intercept attempts to access undefined attributes and customize behavior
+
+# 4. What is the primary difference between the str() and repr() functions when applied to an object?
+# str() returns a string representation of an object for end-users, 
+# while repr() returns a string representation primarily intended for developers.
