21aa02bf1f5c481b379d9fa02ee736cb1b855165
James Allen <jamesba555@yahoo.com>
2024-07-15 14:19:25 -0500

    wip new

--- DataClasses.py
+++ DataClasses.py
@@ -6,7 +6,7 @@
 # Can still add methods the same way as before
 
 # To-dos: print the book itself, compare two dataclasses, and change some fields
-
+'''
 from dataclasses import dataclass
 
 # Instead of this:
@@ -48,3 +48,70 @@ print(b1 == b2)
 b1.title = "Anna Karenina"
 b1.pages = 864
 print(b1.bookinfo())
+'''
+
+# Using Post Initialization:
+
+# Can create attributes based on the value of other attributes
+
+# To-dos: use the __post_init__ function to customize additional properties after the object has been initialized via built-in __init__
+#       and use the description attribute created in the __post_init__ function
+
+'''
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    title: str
+    author: str
+    pages: int
+    price: float
+
+    def __post_init__(self):
+        self.description = f"{self.title} by {self.author}, {self.pages} pages"
+
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+
+print(b1.description)
+print(b2.description)
+'''
+
+
+# Using Default Values
+
+# Data classes provide the ability to define default values for their attributes, subject to some rules
+# you can define default values when attributes are declared
+# Attributes without default values have to come (be listed) first
+
+# Another way of defining a default value is by using the field function
+
+from dataclasses import dataclass, field
+
+# for price_func
+import random
+
+def price_func():
+    return float(random.randrange(20,40))
+
+@dataclass
+class Book:
+    title: str = "No Title"
+    author: str = "No Author"
+    pages: int = 0
+    # The line below can't be a default value beacuse there are line before it that are default values and code won't work
+    # price: float
+    # price: float = 0.0
+    # Line below is another way of setting a default value, field() offers other uses and flexibility
+    # price: float = field(default=10.0)
+    # line below uses price_func function to generate a random price for the book
+    # Each time it is ran in will generate a different price
+    price: float = field(default_factory=price_func)
+
+b1 = Book()
+print(b1)
+
+b2 = Book("War and Peace", "Leo Tolstoy", 1225)
+b3 = Book("The Catcher in the Rye", "JD Salinger", 234)
+print(b2)
+print(b3)
