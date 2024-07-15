831354cb630f0a630ac005d4cd8f55dcb39823e8
James Allen <jamesba555@yahoo.com>
2024-07-15 17:27:53 -0500

    wip

---
+++ DataClassesQuiz.py
@@ -0,0 +1,18 @@
+# Data Classes Quiz
+
+# 1. What is the name of the method to define if you want to perform additional initialization on a data class?
+# post_init
+
+# 2. Which of these is the primary advantage of using the @dataclass decorator in Python?
+# It automatically generates special methods like init and repr for the class.
+
+# 3. What is the correct way of declaring a data class that is immutable?
+# @dataclass(frozen=True)
+
+# 4. 
+# @dataclass 
+# class Person:
+#   occupation : str
+#   name : str = "John Doe"
+#   age : int = 35
+#   city : str = "New York"
